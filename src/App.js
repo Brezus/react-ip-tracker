ae76b66088805a81435f297f227dec68c5a01cd0
Roshane Miller <millerroshane44@gmail.com>
2022-06-06 09:35:20 -0400

    experimenting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React from "react";
 import InputDisplay from "./InputDisplay";
 import "./App.css";
-import LeafletMap from "./components/LeafletMap";
+import axios from "axios";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 function App() {
   const [ipInfo, setIpInfo] = React.useState("");
   const [ip, setIp] = React.useState("");
   const [isLoading, setIsLoading] = React.useState("false");
+  const [latLon, setLatLon] = React.useState(null);
+  const [lat, setLat] = React.useState(37.7749);
+  const [lng, setLng] = React.useState(-122.4194);
+  const [zoom, setZoom] = React.useState(13);
 
   function getIp(event) {
+    // get the ip typed in by user
     setIp(event.target.value);
   }
 
   async function getAddressInfo() {
     let apiUrl = `https://geo.ipify.org/api/v2/country?apiKey=at_oNvwKc6F4I37eCbKER4WSoYriVKGY&ipAddress=${ip}`;
-    setIsLoading(true);
+    // setIsLoading(true);
     try {
-      const promise = await fetch(apiUrl);
-      if (!promise.ok) {
-        throw new Error(`Error! status: ${promise.status}`);
-      } else {
-        const response = promise.json();
-        setIpInfo(response);
-        console.log(ipInfo);
-      }
+      const result1 = await axios.get(apiUrl);
+      setIpInfo(result1);
+      let secondApiUrl = `http://open.mapquestapi.com/geocoding/v1/address?key=O5jXHb2AGA7PGVJm07TRI8BSGVsaROwa&location=`;
+      const result2 = await axios.get(
+        secondApiUrl + result1.data.location.region
+      );
+      // call open maps api passing the location recieved from calling geolocation api with ip address
+      setLatLon(result2.data.results[0].locations[0].displayLatLng);
+      console.log(result2);
     } catch (error) {
       console.log(error);
     } finally {
@@ -38,7 +61,19 @@ function App() {
         handleClick={getAddressInfo}
         isLoading={isLoading}
       />
-      <LeafletMap />
+      {latLon && (
+        <MapContainer
+          center={[latLon.lat, latLon.lng]}
+          zoom={zoom}
+          //   whenCreated={map => setPosition({ map })}
+          style={{ width: "100%", height: "900px" }}
+        >
+          <TileLayer
+            attribution='&copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      )}
     </div>
   );
 }
--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,25 +1,48 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+// import React from "react";
+// import {
+//   MapContainer,
+//   TileLayer,
+//   Marker,
+//   Popup,
+//   useMapEvents,
+// } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+// import L from "leaflet";
 
-export default function LeafletMap() {
-  const [lat, setLat] = React.useState(37.7749);
-  const [lng, setLng] = React.useState(-122.4194);
-  const [zoom, setZoom] = React.useState(13);
+// delete L.Icon.Default.prototype._getIconUrl;
 
-  return (
-    <MapContainer
-      center={[lat, lng]}
-      zoom={zoom}
-      style={{ width: "100%", height: "900px" }}
-    >
-      <TileLayer
-        attribution='&copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
-}
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+//   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+//   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+// });
 
-get make fetch api to get data on the result of 
+// export default function LeafletMap(props) {
+//   const [lat, setLat] = React.useState(37.7749);
+//   const [lng, setLng] = React.useState(-122.4194);
+//   const [zoom, setZoom] = React.useState(13);
 
+//   return (
+//     <MapContainer
+//       center={[lat, lng]}
+//       zoom={zoom}
+//       //   whenCreated={map => setPosition({ map })}
+//       style={{ width: "100%", height: "900px" }}
+//     >
+//       <TileLayer
+//         attribution='&copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={[lat, lng]}>
+//         <Popup>bruh</Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// }
 
+// // make fetch api to get data on the result of ipInfo
+// // fetch data from http://open.mapquestapi.com/geocoding/v1/address?key=O5jXHb2AGA7PGVJm07TRI8BSGVsaROwa&location=ipInfo.address
+// // pass it the result of the address recieved from the ipInfo.CityName
+// // then save those results in a state specifically the lattitude and longitude returned
+// // then save that lat and lon in an array
+// // pass that array as props to leaflet component
